27250ff030227f2545f13439f7403bda467f2c6f
AEmin96 <maxxseed00@yahoo.com>
2023-03-15 20:13:25 +0000

    Fixing validator issues and added action page for form

---
+++ formaction.php
@@ -0,0 +1,17 @@
+<form method="get" action="/formaction.php" id="formbox">
+        <label for="fname" class="labels">First Name : </label>
+        <br>
+        <input type="text" name="fname" id="fname" > 
+        <br>
+        <label for="lname" class="labels">Last Name : </label>
+        <br>
+        <input type="text" name="lname" id="lname">
+                <br>
+        <label for="email" class="labels">Email Address : </label>
+        <br>
+        <input type="email" name="email" id="email">
+        <br>
+        <spam class="labels">Describe Your Hack Below: </spam>
+        <textarea id="textarea"></textarea> <br>
+        <input type="submit" id="submit">
+        </form>
